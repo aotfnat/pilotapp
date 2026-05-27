97015f4a7af104487b121d5cdf578263ff67f5f7
aotfnat <aotfnat@yahoo.com>
2026-05-26 20:39:15 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'pilotwx-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'app.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('aviationweather.gov')) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return fetch(event.request).then(response => {
+          cache.put(event.request, response.clone());
+          return response;
+        }).catch(() => cache.match(event.request));
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
+});
