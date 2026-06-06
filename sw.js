b8ddcee5a48e3c2647b0eec72e3ee6dcefd606c0
aotfnat <aotfnat@yahoo.com>
2026-06-05 23:20:13 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pilotwx-v2.4';
+const CACHE = 'pilotwx-v2.5';
 
 // Derive the correct path to index.html relative to wherever sw.js is deployed.
 // e.g. if sw.js is at https://example.com/pilotapp/sw.js,
